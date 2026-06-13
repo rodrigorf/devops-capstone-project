c1c4ea86f5b574815770f304530a1215bbd4656f
rodrigorf <rodrigorf33@gmail.com>
2026-06-13 16:15:28 -0400

    fix test route

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -27,6 +27,8 @@ HTTPS_ENVIRON = {
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -211,6 +213,4 @@ class TestAccountService(TestCase):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-         
